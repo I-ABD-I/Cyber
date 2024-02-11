e143976bfc71e5e2e0d793b9d70159c28c6d6618
Aylon Ben Dvora <70889286+I-ABD-I@users.noreply.github.com>
2024-02-11 14:59:43 +0200

    Update secret message client and server

--- network/scapy/6.13/secret_msg_client.py
+++ network/scapy/6.13/secret_msg_client.py
@@ -7,7 +7,7 @@ id = 0
 
 def main():
     global id
-    msg = "Hello World!"
+    msg = input("Please enter a msg to send: ")
     for c in msg:
         pkt = IP(dst=IP_ADDR) / UDP(sport=id, dport=ord(c))
         id += 1
--- network/scapy/6.13/secret_msg_server.py
+++ network/scapy/6.13/secret_msg_server.py
@@ -1,17 +1,28 @@
 from scapy.all import sniff, Packet, conf, Raw
 from scapy.layers.inet import UDP
+from scapy.layers.dns import DNS
 
 
 def filter_emtpy(pkt: Packet):
-    return UDP in pkt and Raw not in pkt
+    return UDP in pkt and Raw not in pkt and DNS not in pkt
 
 
+seen = set()
 chars = []
 
 
 def print_char(pkt: Packet):
-    chars.insert(pkt[UDP].sport, chr(pkt[UDP].dport))
-    print("".join(chars), end="\r")
+    global seen, chars
+    if pkt[UDP].sport not in seen:
+        seen.add(pkt[UDP].sport)
+        chars.insert(pkt[UDP].sport, chr(pkt[UDP].dport))
+        print("".join(chars), end="\r")
+    else:
+        seen = set()
+        chars = []
+        seen.add(pkt[UDP].sport)
+        chars.insert(pkt[UDP].sport, chr(pkt[UDP].dport))
+        print()
 
 
 def main():
